5628ec0b5138feafdc916be9cb1f5209afd7c105
bhanubhargavbhas <bhanuprakash339@gmail.com>
2022-03-05 14:26:48 +0530

    General

--- key_pair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "main" {
-  key_name   = "code-deploy-demo"
-  public_key = "${file(var.public_key_path)}"
-}
--- main.tf
+++
@@ -1,7 +0,0 @@
-data "aws_ami" "amzn-linux" {
-  most_recent = true
-  filter {
-    name = "name"
-    values = ["amzn-ami-*-x86_64-gp2"]
-  }
-}
